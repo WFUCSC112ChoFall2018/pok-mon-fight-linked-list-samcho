d0bc8d23f31faad91df704d243e5bd143c6f8b25
Samuel Cho <choss@wfu.edu>
2023-10-05 15:51:23 -0400

    Updated find, insert, and pop methods

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -30,17 +30,54 @@ public class LinkedList {
     }
 
     // find a node, return whether found or not
-    public boolean find(Pokemon mon) {
-        // FINISH ME
+    public Node find(Pokemon mon) {
+        Node runner = head;
+        // check if empty list
+        if (runner != null) {
+          while (runner.next != null) {
+              if(runner.mon.compareTo(mon) == 0) {
+                  return runner; // found node with Pokémon
+              } else {
+                  runner = runner.next; // go to next node
+              }
+          }
+          return null; // does not exist in list
+        } else {
+            return null; // empty list
+        }
     }
 
     // Insert a node at the end of the list
     public void insert(Pokemon mon) {
-        // FINISH ME
+        // create and initialize a node with parameter mon
+        Node newNode = new Node();
+        newNode.mon = mon;
+
+        if (head == null) { // empty list
+            head = newNode; // create a list with one mon
+        } else {
+            // start off at beginning
+            Node runner = head.next;
+            Node previous = head;
+            // while not at end of list
+            while (runner != null) {
+                // traverse list
+                previous = runner;
+                runner = runner.next;
+            }
+            // insert node into list
+            previous.next = newNode;
+        }
     }
 
     // Delete a node at the beginning of the Linked List and return it
     public Pokemon pop () {
-        // FINISH ME
+        // if empty list
+        if (head == null) {
+            return null;
+        }
+        Node temp = head; // saving the node at head
+        head = head.next; // moving the head over one
+        return temp.mon; // returning the mon at the saved node
     }
 }
